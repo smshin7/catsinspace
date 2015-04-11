fab2ce5a593ffaf6faa38969fcd4a6103494891d
gogojojo <joseph.gochua@gmail.com>
2015-04-11 16:39:30 -0700

    added more styling

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -3,4 +3,14 @@ $(function(){
     $('.post').removeClass('hidden')
     $('.navigation').addClass('hidden')
   })
+  $('.menu').click(function(){
+    $('.menu').css('visibility','hidden');
+    $('.navigation').toggleClass('opennav');
+    
+  })
+  $('.close').click(function(){
+    $('.navigation').toggleClass('opennav');
+    $('.menu').css('visibility','visible');
+  })
+
 })
