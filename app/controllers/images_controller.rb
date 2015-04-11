7a948da8e59ccd9b4258b34c279a49fbe8e3bf23
smshin7 <scottshin1@gmail.com>
2015-04-11 11:15:04 -0700

    added images controller actions

---
+++ app/controllers/images_controller.rb
@@ -0,0 +1,30 @@
+class ImagesController < ApplicationController
+
+  def index
+    @images = Image.all
+  end
+  
+  def show
+    @image = Image.find(params[:id])
+  end
+
+  def new
+    @image = Image.new
+  end
+
+  def create
+    @image = Image.new(image_params)
+
+    if @image.save
+      redirect_to images_path
+    else
+      render '/new'
+    end
+  end
+
+  private
+
+  def image_params
+    params.require(:image).permit(:title, :description, :link, :date, :photo)
+  end
+end
