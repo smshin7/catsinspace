fa78ee91fee482c3ee8b4c0ed00ea6f10b1e4397
gogojojo <joseph.gochua@gmail.com>
2015-04-11 18:02:59 -0700

    finsihed modal

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -3,6 +3,19 @@ $(function(){
     $('.post').removeClass('hidden')
     $('.navigation').addClass('hidden')
   })
+  $('.loginbutton').click(function(){
+    $('.login').removeClass('hidden')
+    $('.navigation').addClass('hidden')
+  })
+   $('.cancel').click(function(){
+    $('.post').addClass('hidden')
+    $('.navigation').removeClass('hidden')
+  })
+   $('.cancellogin').click(function(){
+    $('.login').addClass('hidden')
+    $('.navigation').removeClass('hidden')
+  })
+
   $('.menu').click(function(){
     $('.menu').css('visibility','hidden');
     $('.navigation').toggleClass('opennav');
