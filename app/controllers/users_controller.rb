fe671e81c74bc4743aa56dd786892634269738ed
gogojojo <joseph.gochua@gmail.com>
2015-04-11 20:44:04 -0700

    finished styling flash msgs

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,6 @@ class UsersController < ApplicationController
     user = User.new(params.require(:user).permit(:email, :username, :password)) 
     if user.save
        session['user_id'] = user.id.to_s
-       redirect_to root_path
       flash[:success] = "Welcome to our app" 
       redirect_to root_path
     end
